a82c54369ae6fb07e9e194ac21644634c75ed631
trinhtienquang <trinhtienquang2002@gmail.com>
2025-03-20 09:28:30 +0700

    camera-face test

---
+++ test_face_camera/android/app/src/main/kotlin/com/example/test_face_camera/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.test_face_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ test_face_camera/lib/main.dart
@@ -0,0 +1,59 @@
+import 'dart:io';
+import 'package:face_camera/face_camera.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FaceCamera.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FaceCaptureScreen(),
+    );
+  }
+}
+
+class FaceCaptureScreen extends StatefulWidget {
+  @override
+  State<FaceCaptureScreen> createState() => _FaceCaptureScreenState();
+}
+
+class _FaceCaptureScreenState extends State<FaceCaptureScreen> {
+  late FaceCameraController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = FaceCameraController(
+      autoCapture: true,
+      defaultCameraLens: CameraLens.front,
+      onFaceDetected: (face) => print('Phát hiện khuôn mặt'),
+      onCapture: (File? image) async {
+        if (image != null) {
+          print('Đường dẫn ảnh: ${image.path}');
+        }
+      },
+      
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children:[
+        SmartFaceCamera(
+          controller: controller,
+          message: 'Center your face in the square',
+        ),
+        ]
+      ),
+    );
+  }
+}
